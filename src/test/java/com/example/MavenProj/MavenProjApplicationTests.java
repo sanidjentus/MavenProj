0e7807ee2380ef64d62038177d45e1c741eaf3cd
sanidjentus <zolotuhin00@list.ru>
2020-08-09 22:19:00 +0300

    1-st commit

---
+++ src/test/java/com/example/MavenProj/MavenProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MavenProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
